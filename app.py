7ab6a34998da5dfee18f2989a47473ad765cdaab
AliReza0015-ux <mohs0015@algonquinlive.com>
2025-08-04 04:28:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 from utils import load_data, train_kmeans, calculate_wcss, calculate_silhouette
